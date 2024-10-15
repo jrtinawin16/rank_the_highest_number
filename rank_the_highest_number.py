7fc3b38f5b643a0d09edc27a38fdb6ca5f99e81c
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2024-10-15 23:37:37 +0800

    Polished the needed revisions for the conditions

--- rank_the_highest_number.py
+++ rank_the_highest_number.py
@@ -14,8 +14,10 @@
     # compare random_num3 to random_num5 
     # if random_num3 is greater than random_num5
         # print random_num3
-# If not 
-        # print random_num5
+# If random_num4 is greater than random_num5
+        # print random_num4
+# if not
+    #print random_num5
 
 random_num1 = int(input("Please give first number: "))
 random_num2 = int(input("Please give second number: "))
@@ -28,15 +30,19 @@ def find_highest(random_num1, random_num2, random_num3, random_num4, random_num5
             return random_num1
         else:
             return random_num3
-    elif random_num2 > random_num3:
+    if random_num2 > random_num3:
         if random_num2 > random_num4:
             return random_num2
         else:
             return random_num4
-    elif random_num3 > random_num4:
+    if random_num3 > random_num4:
         if random_num3 > random_num5:
             return random_num3
-    else: 
+        else: 
             return random_num5   
+    if random_num4 > random_num5:
+        return random_num4
+    else:
+        return random_num5
 result = find_highest(random_num1,random_num2,random_num3,random_num4,random_num5)
 print(f"The highest number is: {result}")
