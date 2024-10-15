312f44920fd5c7fe731768fbb9be4a8666f79d2a
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2024-10-15 21:50:01 +0800

    Working on the next part of pseudocode

---
+++ rank_the_highest_number.py
@@ -0,0 +1,19 @@
+# Give prompt to user to input 5 random numbers
+# Compare random_num1, random_num2, random_num3, random_num4, & random_num5
+# If random_num1 is greater than random_num2
+    # compare random_num1 to random_num3
+    # if random_num1 is greater than random_num3
+        # print random_num1
+    # if not print random_num3
+# If not
+    # compare random_num2 to random_num3
+    # if random_num2 is greater than random_num3
+        # print random_num2
+# If not 
+    # compare random_num 
+
+
+
+
+# If not 
+        # print random_num5
