f7329815fc1a7fa3092d15a6484777d056b57041
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2024-10-18 00:20:03 +0800

    Further modified the pseudocode

--- rank_the_highest_number.py
+++ rank_the_highest_number.py
@@ -1,9 +1,15 @@
 # Give prompt to user to input 5 random numbers
 # Compare random_num1, random_num2, random_num3, random_num4, & random_num5
 # If random_num1 is greater than or equal to random_num2 and random_num1 is greater than or equal to random_num3
-            # print random_num1
-# If random_num2 is greater or equal to random_num1 and random_num2 is greater than or equal to random_num3
-        # print random_num2
+    # print random_num1
+# If random_num2 is greater than or equal to random_num1 and random_num2 is greater than or equal to random_num3
+    # print random_num2
+# If random_num3 is greater than or equal to random_num2 and random_num3 is greater than or equal to random_num4
+    # print random_num3
+# If random_num4 is greater than or equal to random_num3 and random_num4 is greater than or equal to random_num5
+    # print random_num4
+# If not
+    # print random_num5
 
 
 
@@ -23,11 +29,9 @@ random_num3 = int(input("Please give third number: "))
 random_num4 = int(input("Please give fourth number: "))
 random_num5 = int(input("Please give fifth number: "))
 def find_highest(random_num1, random_num2, random_num3, random_num4, random_num5):
-    if random_num1 > random_num2:
-        if random_num1 > random_num3:
+    if random_num1 >= random_num2 and random_num1 >= random_num3:
             return random_num1
-        else:
-            return random_num3
+
     if random_num2 > random_num3:
         if random_num2 > random_num4:
             return random_num2
